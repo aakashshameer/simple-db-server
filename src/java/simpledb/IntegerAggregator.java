7e18562c80a35f0c6b852003a0c122d4df901588
Aakash Srazali <aaksra@cs.washington.edu>
2023-04-19 00:19:48 -0700

    Finished IntegerAggregator.java

--- src/java/simpledb/IntegerAggregator.java
+++ src/java/simpledb/IntegerAggregator.java
@@ -1,11 +1,25 @@
 package simpledb;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Knows how to compute some aggregate over a set of IntFields.
  */
 public class IntegerAggregator implements Aggregator {
 
     private static final long serialVersionUID = 1L;
+
+    private int gbfield;
+    private Type gbfieldtype;
+    private int afield;
+    private Op what;
+
+    private String afieldName;
+    private String gbfieldName;
+    private final Map<Field, Integer> fieldCount;
+    private final Map<Field, Integer> fieldAggregate;
 
     /**
      * Aggregate constructor
@@ -24,6 +38,13 @@ public class IntegerAggregator implements Aggregator {
 
     public IntegerAggregator(int gbfield, Type gbfieldtype, int afield, Op what) {
         // some code goes here
+        this.gbfield = gbfield;
+        this.gbfieldtype = gbfieldtype;
+        this.afield = afield;
+        this.what = what;
+
+        this.fieldCount = new HashMap<>();
+        this.fieldAggregate = new HashMap<>();
     }
 
     /**
@@ -35,6 +56,51 @@ public class IntegerAggregator implements Aggregator {
      */
     public void mergeTupleIntoGroup(Tuple tup) {
         // some code goes here
+        this.afieldName = tup.getTupleDesc().getFieldName(this.afield);
+        int aggregate = ((IntField) tup.getField(this.afield)).getValue();
+
+        if (gbfield != NO_GROUPING) {
+            this.gbfieldName = tup.getTupleDesc().getFieldName(this.gbfield);
+            mergeTupleIntoGroup(tup.getField(this.gbfield), aggregate);
+
+        } else {
+            // if no grouping, use null keys
+            mergeTupleIntoGroup(null, aggregate);
+        }
+    }
+
+    /**
+     * Helper method for merging tuple into group
+     */
+    private void mergeTupleIntoGroup(Field gbfield, int aggregate) {
+        // Store aggregate if not yet in map
+        if (!this.fieldAggregate.containsKey(gbfield)) {
+            this.fieldAggregate.put(gbfield, aggregate);
+        } else {
+            int currAggregate = this.fieldAggregate.get(gbfield);
+            switch (this.what) {
+                // perform different actions based on aggregate operator received
+                case COUNT:
+                    break;  // do nothing
+                case SUM:
+                case AVG:
+                    // SUM same case as AVG; for AVG, take the AVG later
+                    this.fieldAggregate.put(gbfield, currAggregate + aggregate);
+                    break;
+                case MIN:
+                    this.fieldAggregate.put(gbfield, Math.min(currAggregate, aggregate));
+                    break;
+                case MAX:
+                    this.fieldAggregate.put(gbfield, Math.max(currAggregate, aggregate));
+                    break;
+                default:
+                    System.err.println("mergeTupleIntoGroup ran into an error");
+                    break;
+            }
+        }
+
+        // Increment count map by 1
+        this.fieldCount.put(gbfield, this.fieldCount.getOrDefault(gbfield, 0) + 1);
     }
 
     /**
@@ -47,8 +113,60 @@ public class IntegerAggregator implements Aggregator {
      */
     public OpIterator iterator() {
         // some code goes here
-        throw new
-        UnsupportedOperationException("please implement me for lab2");
+        ArrayList<Tuple> tuplesArr = new ArrayList<>();
+        if (gbfield != NO_GROUPING) {
+            // Form TupleDesc
+            // name of aggregate column format = aggregate op, aggregate field
+            TupleDesc td = new TupleDesc(new Type[]{this.gbfieldtype, Type.INT_TYPE},
+                    new String[]{this.gbfieldName, this.what.toString() + ", " + this.afieldName});
+
+            // create tuples
+            for (Field f : fieldAggregate.keySet()) {
+
+                Tuple tup = new Tuple(td);
+                tup.setField(0, f);
+                tup.setField(1, new IntField(getAggregate(f)));
+                tuplesArr.add(tup);
+            }
+
+            // Create TupleIterator
+            return new TupleIterator(td, tuplesArr);
+
+        } else {
+            // NO_GROUPING case
+            // Form TupleDesc
+            Type[] typeAr = new Type[]{Type.INT_TYPE};
+            String[] fieldAr = new String[]{this.what.toString() + " " + this.afieldName};
+            TupleDesc td = new TupleDesc(typeAr, fieldAr);
+
+            Tuple tup = new Tuple(td);
+            tup.setField(0, new IntField(getAggregate(null)));
+            tuplesArr.add(tup);
+            return new TupleIterator(td, tuplesArr);
+
+        }
+    }
+
+    private int getAggregate(Field f) {
+        int aggregate = -1;
+        switch (this.what) {
+            case COUNT:
+                aggregate = this.fieldCount.get(f);
+                break;
+            case SUM:
+            case MIN:
+            case MAX:
+                aggregate = this.fieldAggregate.get(f);
+                break;
+            case AVG:
+                aggregate = this.fieldAggregate.get(f) / this.fieldCount.get(f);
+                break;
+            default:
+                System.err.println("getAggregate ran into an error");
+                break;
+        }
+        return aggregate;
+
     }
 
 }
